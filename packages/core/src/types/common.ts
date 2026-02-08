cb33498df81775c825252ac4ddee3674fde2d05b
Jongmin Lee <cyan@northprot.com>
2026-02-08 02:53:36 -0800

    Revert "Merge branch 'div/dev-core'"
    This reverts commit feadba55ba6ada1e9167b68a8a1ad4ee83ff3362, reversing
    changes made to 9a7777b572301bad562c916ea92ebbbc11cc1a8f.

--- packages/core/src/types/common.ts
+++
@@ -1,7 +0,0 @@
-/** Token usage tracking for LLM calls */
-export interface TokenUsage {
-	readonly inputTokens: number;
-	readonly outputTokens: number;
-	readonly cacheReadTokens: number;
-	readonly cacheCreationTokens: number;
-}
