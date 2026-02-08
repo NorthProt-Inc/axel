4c216bc9b9c528a09285a27483466921e2f70cc9
Jongmin Lee <cyan@northprot.com>
2026-02-08 03:07:49 -0800

    Revert "Merge branch 'div/dev-core'"
    This reverts commit 23fa80ca18d5ccad5d9265b2316f6d05d871ef52, reversing
    changes made to 659cd259cc76c6daf74fe0963eefb6514e19d864.

--- packages/core/src/decay/index.ts
+++
@@ -1,8 +0,0 @@
-export { calculateDecayedImportance } from './calculator.js';
-export { decayBatch } from './batch.js';
-export {
-	DecayInputSchema,
-	DecayConfigSchema,
-	DEFAULT_DECAY_CONFIG,
-} from './types.js';
-export type { DecayInput, DecayConfig } from './types.js';
--- packages/core/src/persona/index.ts
+++
@@ -1,6 +0,0 @@
-export { PersonaSchema } from './schema.js';
-export type { Persona } from './schema.js';
-export { buildSystemPrompt } from './engine.js';
-export type { PersonaEngine } from './engine.js';
-export { CHANNEL_ADAPTATIONS, ChannelAdaptationSchema } from './channel-adaptations.js';
-export type { ChannelAdaptation } from './channel-adaptations.js';
